dfbf9292c3e013ab89af52fba2c3cc00751937b2
octa13 <octa_13@hotmail.com>
2025-02-24 00:08:49 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-
+document.addEventListener("scroll", function() {
+    let contacto = document.getElementById("contacto");
+    let whatsappButton = document.getElementById("whatsapp-button");
+    
+    // Obtiene la posición de la sección "contacto"
+    let contactoPosition = contacto.getBoundingClientRect().top;
+    
+    // Si la sección "contacto" está visible en la pantalla, mostramos el botón
+    if (contactoPosition < window.innerHeight && contactoPosition > 0) {
+        whatsappButton.style.display = "block";
+    } else {
+        whatsappButton.style.display = "none";
+    }
+});
